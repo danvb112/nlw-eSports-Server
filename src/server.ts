347e305cc0a076a88b511d9b9a3af5c62bd45cfc
danvb112 <danielbonasser@gmail.com>
2022-09-15 07:28:01 -0300

    Create function to get all games

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
 import express from 'express';
+import {PrismaClient} from '@prisma/client';
 
 const app = express();
 
-app.get('/games', (request, response) => {
-    return response.json([]);
+const prisma = new PrismaClient({
+    log: ['query']
+})
+
+app.get('/games', async (request, response) => {
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    Ad: true
+                }
+            }
+        }
+    })
+
+    return response.json(games);
 });
 
 app.post('/ads', (request, response) => {
