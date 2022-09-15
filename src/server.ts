87abc1740a2a1c3b3d9b10433805a633eaf0ef76
danvb112 <danielbonasser@gmail.com>
2022-09-15 08:23:00 -0300

    Use Cors in app

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import {PrismaClient} from '@prisma/client';
 import { convertHourStringToMinutes, convertMinutesToHourString } from './utils/string';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const prisma = new PrismaClient({
     log: ['query']
