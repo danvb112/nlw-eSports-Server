d21c290cc35e500ba6f43d5bd10449446f7406d3
danvb112 <danielbonasser@gmail.com>
2022-09-15 07:32:39 -0300

    Create function to get ad by gameId

--- src/server.ts
+++ src/server.ts
@@ -25,13 +25,33 @@ app.post('/ads', (request, response) => {
     return response.status(201).json([]);
 });
 
-app.get('/games/:id/ads', (request, response) => {
-    response.json([
-       {id: 1, name: 'Anuncio 1'}, 
-       {id: 2, name: 'Anuncio 2'}, 
-       {id: 3, name: 'Anuncio 3'}, 
-       {id: 4, name: 'Anuncio 4'}, 
-    ])
+app.get('/games/:id/ads', async (request, response) => {
+    const gameId = request.params.id;
+
+    const ads = await prisma.ad.findMany({
+        select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            useVoiceChannel: true,
+            yearsPlaying: true,
+            hourStart: true,
+            hourEnd: true,
+        },
+        where: {
+            gameId
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+
+    response.json(ads.map(ad => {
+        return {
+            ...ad,
+            weekDays: ad.weekDays.split(',')
+        }
+    }))
 });
 
 app.get('/ads/:id/discrod', (request, response) => {
